7640cd8589921492618be58204891d6f406dea89
JAGERCZECH <jagerczechcorp@gmail.com>
2025-01-01 22:35:50 +0000

    1735770950

--- W/WEB/homepage/app.py
+++ W/WEB/homepage/app.py
@@ -6,7 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     rss_feed = feedparser.parse('http://feeds.bbci.co.uk/news/world/rss.xml')
-    articles = rss_feed.entries
+    articles = []
+    for entry in rss_feed.entries:
+        article = {
+            'title': entry.title,
+            'link': entry.link,
+            'summary': entry.summary,
+            'media_content': entry.get('media_content', []),
+            'media_thumbnail': entry.get('media_thumbnail', []),
+        }
+        print(f"Article: {article['title']}")
+        print(f"Media Content: {article['media_content']}")
+        print(f"Media Thumbnail: {article['media_thumbnail']}")
+        articles.append(article)
     return render_template('index.html', articles=articles)
 
 if __name__ == '__main__':
