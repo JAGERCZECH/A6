5e674983b8de9c5cd488a8566c7857a7cf5ab791
JAGERCZECH <jagerczechcorp@gmail.com>
2025-01-01 22:01:45 +0000

    1735768905

--- W/WEB/homepage/app.py
+++ W/WEB/homepage/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import feedparser
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    rss_feed = feedparser.parse('http://feeds.bbci.co.uk/news/world/rss.xml')
+    articles = rss_feed.entries
+    return render_template('index.html', articles=articles)
 
 if __name__ == '__main__':
     app.run(debug=True)
